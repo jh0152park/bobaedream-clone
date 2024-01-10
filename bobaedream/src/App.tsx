94412e7729a6d99f1e2b419cff91335371360f12
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-10 18:53:11 +0900

    created header

--- bobaedream/src/App.tsx
+++ bobaedream/src/App.tsx
@@ -1,5 +1,24 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Notfound from "./screens/Notfound";
+import Main from "./screens/Main";
+import Layout from "./screens/Loyout";
+
+const route = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <Notfound />,
+        children: [
+            {
+                path: "",
+                element: <Main />,
+            },
+        ],
+    },
+]);
+
 function App() {
-    return <div></div>;
+    return <RouterProvider router={route}></RouterProvider>;
 }
 
 export default App;
--- bobaedream/src/index.tsx
+++ bobaedream/src/index.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
+import { reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    background-color: white;
+    color: black;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
+  }
+
+  * {
+    text-decoration: none;
+    box-sizing: border-box;
+  }
+`;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+    <>
+        <GlobalStyle />
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </>
+);
