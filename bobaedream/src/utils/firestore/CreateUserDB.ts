3c5603e6090cc0b197bd00182f6bce7599db02e8
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-12 00:18:48 +0900

    created protect overlap nickname logic

---
+++ bobaedream/src/utils/firestore/CreateUserDB.ts
@@ -0,0 +1,49 @@
+/*
+
+name: ""
+Created at: ""
+Post: []
+Comment: []
+
+*/
+
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { FirebaseDB } from "../../Firebase";
+
+export async function CreateUserDB(
+    name: string,
+    createAt: string,
+    uid: string
+) {
+    await addDoc(collection(FirebaseDB, uid), {
+        name: name,
+        createAt: createAt,
+        post: [],
+        comment: [],
+    });
+}
+
+export async function CreateUserAllNameDB(name: string) {
+    await addDoc(collection(FirebaseDB, "username"), {
+        name: name,
+    });
+}
+
+export async function IsExistUserName(name: string) {
+    try {
+        const querySnapshot = await getDocs(collection(FirebaseDB, "username"));
+        querySnapshot.forEach((doc) => {
+            // console.log(doc.id, " => ", doc.data());
+            console.log(
+                `name from db: ${doc.data()["name"]} / input name: ${name}`
+            );
+            if (doc.data()["name"] === name) {
+                console.log("same name already exists");
+                throw new Error("escape loop");
+            }
+        });
+        return false;
+    } catch (e) {
+        return true;
+    }
+}
