6b521816c8fe7d4e8a7699174b42b8e68c025f79
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-11 19:52:39 +0900

    removed useless code

--- bobaedream/src/utils/accounts/CreateAccount.ts
+++ bobaedream/src/utils/accounts/CreateAccount.ts
@@ -1,23 +1,18 @@
-import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    updateProfile,
-} from "firebase/auth";
-import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { FirebaseAuth } from "../../Firebase";
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
 
-const FirebaseApp = initializeApp(firebaseConfig);
+// const FirebaseApp = initializeApp(firebaseConfig);
 
-const auth = getAuth();
+// const auth = getAuth();
 
 export async function CreateAccount(
     email: string,
@@ -26,7 +21,7 @@ export async function CreateAccount(
 ) {
     try {
         const credential = await createUserWithEmailAndPassword(
-            auth,
+            FirebaseAuth,
             email,
             password
         );
--- bobaedream/src/utils/accounts/SignInAccount.ts
+++ bobaedream/src/utils/accounts/SignInAccount.ts
@@ -1,28 +1,24 @@
-import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    updateProfile,
-} from "firebase/auth";
-import { initializeApp } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+import { FirebaseAuth } from "../../Firebase";
 
-const FirebaseApp = initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
 
-const auth = getAuth();
+// const FirebaseApp = initializeApp(firebaseConfig);
+
+// const auth = getAuth();
 
 export async function SignInAccount(email: string, password: string) {
     try {
         const credential = await signInWithEmailAndPassword(
-            auth,
+            FirebaseAuth,
             email,
             password
         );
