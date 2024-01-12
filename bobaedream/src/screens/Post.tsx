055628f05d45005d3794ed211e18b0e982e20918
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-12 17:39:47 +0900

    added submit function

--- bobaedream/src/screens/Post.tsx
+++ bobaedream/src/screens/Post.tsx
@@ -5,11 +5,38 @@ import {
     Input,
     Textarea,
     VStack,
+    useToast,
 } from "@chakra-ui/react";
+import { FieldValues, useForm } from "react-hook-form";
 
 export default function Post() {
+    const toast = useToast();
+    const { register, reset, handleSubmit } = useForm();
+
+    function onSubmit(data: FieldValues) {
+        console.log(data);
+        if (!data.title) {
+            toast({
+                status: "error",
+                title: "제목을 입력하세요",
+                description: "게시글의 제목은 필수입니다.",
+                position: "top",
+            });
+            return;
+        }
+        if (!data.content) {
+            toast({
+                status: "error",
+                title: "내용을 입력하세요",
+                description: "게시글의 내용은 필수입니다.",
+                position: "top",
+            });
+            return;
+        }
+    }
+
     return (
-        <VStack w="100%" h="85vh">
+        <VStack w="100%" h="85vh" as="form" onSubmit={handleSubmit(onSubmit)}>
             <HStack
                 w="100%"
                 h="50px"
@@ -23,6 +50,7 @@ export default function Post() {
                     borderRadius="5px"
                     border="1px solid rgba(0, 0, 0, 0.3)"
                     _hover={{ cursor: "pointer" }}
+                    as="button"
                 >
                     파일첨부
                 </Center>
@@ -33,6 +61,7 @@ export default function Post() {
                     borderRadius="5px"
                     border="1px solid rgba(0, 0, 0, 0.3)"
                     _hover={{ cursor: "pointer" }}
+                    as="button"
                 >
                     게시하기
                 </Center>
@@ -51,6 +80,7 @@ export default function Post() {
                     color="black"
                     fontSize="15px"
                     bgColor="white"
+                    {...register("title", { required: false })}
                 />
                 <Textarea
                     w="100%"
@@ -65,6 +95,7 @@ export default function Post() {
                         color: "black",
                         fontSize: "15px",
                     }}
+                    {...register("content", { required: false })}
                 />
             </FormControl>
         </VStack>
