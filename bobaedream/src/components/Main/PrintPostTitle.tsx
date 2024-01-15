a1f07d698527ba58da3ef8492728942087517b26
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-15 18:49:25 +0900

    added preview

--- bobaedream/src/components/Main/PrintPostTitle.tsx
+++ bobaedream/src/components/Main/PrintPostTitle.tsx
@@ -1,8 +1,12 @@
+import { HStack, Text } from "@chakra-ui/react";
+
 interface IPorps {
     order: number;
     title: string;
     author: string;
     postedAt: string;
+    content: string;
+    comments: string[];
 }
 
 export default function PrintPostTitle({
@@ -10,4 +14,30 @@ export default function PrintPostTitle({
     title,
     author,
     postedAt,
-}: IPorps) {}
+    content,
+    comments,
+}: IPorps) {
+    return (
+        <HStack
+            w="100%"
+            h="30px"
+            bgColor="whtie"
+            borderBottom="1px solid rgba(0, 0, 0, 0.5)"
+            fontSize="12px"
+            _hover={{ cursor: "pointer" }}
+        >
+            <Text w="15%" textAlign="center">
+                {order}
+            </Text>
+            <Text w="50%" textAlign="center">
+                {title}
+            </Text>
+            <Text w="15%" textAlign="center">
+                {author}
+            </Text>
+            <Text w="20%" textAlign="center">
+                {postedAt}
+            </Text>
+        </HStack>
+    );
+}
